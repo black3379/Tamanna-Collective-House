67a3ab3ca9e89ff78152096acdd46f8f99c8c6d1
black3379 <amberseaton70@gmail.com>
2025-07-01 13:24:53 -0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppRouter from './App.jsx';
 import './index.css';
+import AppRouter from './App.jsx'; // Import the main App component
 
+// Render the application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppRouter />
